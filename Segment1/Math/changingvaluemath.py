50ea6ed6dcd71ed0a9162c5726ce5d9b148ede90
Jxck-S <JackHSweeney1@gmail.com>
2018-06-21 13:56:11 -0400

    Updating changingvaluemath.py

---
+++ Segment1/Math/changingvaluemath.py
@@ -0,0 +1,12 @@
+# Changing a Variable's Value
+
+def main():
+    x = 5
+    print("The value of x is: ")
+    print( x )
+
+    x = x + 10
+    print("The new value of x is: ")
+    print ( x )
+
+main()
