5bb4ec4156d0cae4dbb571f186c995503675d4c2
Jxck-S <JackHSweeney1@gmail.com>
2018-06-21 13:55:01 -0400

    Updating basicmath.py

---
+++ Segment1/Math/basicmath.py
@@ -0,0 +1,14 @@
+# Integer arithmetic
+
+def main():
+    # Assign integer values
+    x = 11
+    y = 5
+
+    # Print arithmetic results
+    print( x + y )
+    print( x - y )
+    print( x * y )
+    print( x / y )
+
+main()
