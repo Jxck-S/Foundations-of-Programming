acd7dffab1e1b40db82a06aa879dc534e9c39f34
Jxck-S <JackHSweeney1@gmail.com>
2018-08-01 13:00:36 -0400

    Updating Elif.py

---
+++ Segment2/If/Elif.py
@@ -0,0 +1,9 @@
+grade = input("What is your grade?")
+if( grade == "A" ):
+    print("Excellent!")
+elif( grade == "B" ):
+    print("Good job!")
+elif( grade == "C" ):
+    print("Nice start. Try to study more next time!")
+else:
+    print("Please reach out to your teacher.")
