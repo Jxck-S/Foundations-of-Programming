71804a3fb1b76ad29d1b5bcceee732baef578ce0
Jxck-S <JackHSweeney1@gmail.com>
2018-08-08 13:31:41 -0400

    Updating IF with OR.py

---
+++ Segment2/If/IF with OR.py
@@ -0,0 +1,23 @@
+# Logical Operators
+
+def main():
+
+    answer = input("Which frozen treat do you like? A) Ice cream or B) Popsicles")
+
+    if( answer == "a" or answer == "A" ):
+        print("  o, ")
+        print(" (~ ) ")
+        print(" \~~/ ")
+        print("  \/")
+        print(" YUMMY!! ")
+    elif( answer == "b" or answer == "B" ):
+        print("    ___ ")
+        print("   // //\ ")
+        print("  // // /| ")
+        print("  // // // ")
+        print(" ( // // ")
+        print("  \_/ // ")
+        print(" / /\_/  ")
+        print(" (_/  Mmm, good!")
+
+main()
