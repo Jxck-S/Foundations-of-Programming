f75f4ee32b921b1ab0fda13a323069df4341ddbc
Jxck-S <JackHSweeney1@gmail.com>
2018-08-14 16:45:25 -0400

    Updating List2.py

---
+++ Segment2/List/List2.py
@@ -0,0 +1,14 @@
+# Lists in Python
+
+def main():
+
+    names = ["Chewy", "Captain Kirk", "Hermione", "Storm", "Buzz"]
+    length = len(names)
+
+    first = names[0]
+    last = names[length -1]
+
+    print(first + " is the first character I want to meet.")
+    print(last + " will be the last character I meet.")
+
+main()
