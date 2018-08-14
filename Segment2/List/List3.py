baba99e6378d48c4283420a35d088166a8b144c8
Jxck-S <JackHSweeney1@gmail.com>
2018-08-14 16:47:41 -0400

    Updating List3.py

---
+++ Segment2/List/List3.py
@@ -0,0 +1,11 @@
+# Lists in Python
+
+def main():
+
+    names = ["Chewy", "Captain Kirk", "Hermione", "Storm", "Buzz"]
+    length = len(names)
+
+    for n in range(0, len(names)):
+        print("Hello, " + names[n] + "!")
+
+main()
