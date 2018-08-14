3c4c629f67e7c27799ac045715a49e2dbe5471ad
Jxck-S <JackHSweeney1@gmail.com>
2018-08-14 16:45:00 -0400

    Updating List1 .py

---
+++ Segment2/List/List1 .py
@@ -0,0 +1,12 @@
+# Lists in Python
+
+def main():
+
+    names = ["Chewy", "Captain Kirk", "Hermione", "Storm", "Buzz"]
+    length = len(names)
+
+    print("At GeekyCon, you have " + str(length) + " characters to find.")
+    print("They are: ")
+    print(names)
+    
+main()
