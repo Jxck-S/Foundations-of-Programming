62661e9d0123b74f903f4accef74a43287ad9179
Jxck-S <JackHSweeney1@gmail.com>
2018-08-30 20:53:44 -0400

    Updating 502_JackSweeney (1) List &.py

---
+++ Segment2/502_JackSweeney (1) List &.py
@@ -0,0 +1,21 @@
+#Jack Sweeney 8/29/18
+#Asks a user what they think a phrase is then prints their guess and prints the encoded and decoded version of the actual phrase . 
+def main():
+	secretPhrase = "When something is important enough, you do it even if the odds are not in your favor.-Elon Musk"
+	userGuess = input("What do you think the secret phrase is?")
+		
+	secretPhraseEncoded = []
+	secretPhraseDecoded = ""
+	#Encoding Original
+	for m in secretPhrase:
+				secretPhraseEncoded.append(ord(m))
+	#Decoding Encoded 
+	for d in secretPhraseEncoded:
+				secretPhraseDecoded = secretPhraseDecoded + chr(d)
+
+	print "Your Guess: " + userGuess
+	print "Encoded Phrase: " + str(secretPhraseEncoded)
+	print "Decoded Phrase: " + secretPhraseDecoded
+	
+
+main()
