56d9cdfb9fd151de52f3fa9c0cc773272c9b8f27
Jxck-S <JackHSweeney1@gmail.com>
2018-08-30 20:37:57 -0400

    Updating Binary1.py

---
+++ Segment2/Number Systems/Binary1.py
@@ -0,0 +1,13 @@
+# Number Systems
+
+def main():
+
+    # Assign a decimal number to d
+    d = 7
+    print("The decimal value is: " + str(d))
+
+    # Convert d to a binary number
+    b = bin(d) 
+    print("The binary value is: " + b)
+
+main()
