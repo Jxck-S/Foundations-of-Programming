e0aec617e1602d0b27de44827f3a4e7517053031
Jxck-S <JackHSweeney1@gmail.com>
2018-09-05 08:16:20 -0400

    Updating Class1.py

---
+++ Classes/Class1.py
@@ -0,0 +1,14 @@
+# Object-oriented Programming
+
+class Contact:
+
+    def __init__(self):
+        # Create a new contact with name and phone.
+        self.name = ""
+        self.phone = ""
+
+def main():
+
+    newContact = Contact()
+
+main()
