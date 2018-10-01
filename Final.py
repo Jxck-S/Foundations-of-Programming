06b5d34e0828f4acb61e7e9fed386ce645c09ec3
Jxck-S <JackHSweeney1@gmail.com>
2018-10-01 16:17:15 -0400

    Updating Final.py

---
+++ Final.py
@@ -0,0 +1,9 @@
+def main():
+    shirtSize = input("Whats your shirt size/[small,medium, large, or extra large]")
+    shirtColor = input("What color shirt do you want?[blue, green, red, or yellow]")
+    shirtDesign = input("What design do you want on your shirt?[chevron, solid, or stripes]")
+    print (" Your Shirt ")
+    print (" Size  Color  Design ")
+    print (shirtSize +" " + shirtColor + " " + shirtDesign)
+
+main()
